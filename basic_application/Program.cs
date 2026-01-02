18053b905c4c3f0dbd7c057fc1a5567cd5252ade
Chanrith Nhen <nhenchanrithofficial@gmail.com>
2026-01-02 10:42:59 +0700

    first-commit

---
+++ basic_application/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        var name = Console.ReadLine();
+        
+        if (name != null) {
+            Console.WriteLine($"My name is {name}.");
+        }
+        else
+        {
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
